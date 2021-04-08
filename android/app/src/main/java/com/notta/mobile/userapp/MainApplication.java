c1609feea8ac07ed7559260d8fba3f14132e9782
Fariz Darmaniar <haromy@192.168.1.10>
2021-04-08 12:03:24 +0700

    testflight feature

--- android/app/src/main/java/com/notta/mobile/userapp/MainApplication.java
+++ android/app/src/main/java/com/notta/mobile/userapp/MainApplication.java
@@ -1,4 +1,4 @@
-package com.notta_mobile;
+package com.notta.mobile.userapp;
 
 import android.app.Application;
 import android.content.Context;
@@ -63,7 +63,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.notta_mobile.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.notta.mobile.userapp.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
--- ios/NottaTests/NottaTests.m
+++ ios/NottaTests/NottaTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface notta_mobileTests : XCTestCase
+@interface NottaTests : XCTestCase
 
 @end
 
-@implementation notta_mobileTests
+@implementation NottaTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
