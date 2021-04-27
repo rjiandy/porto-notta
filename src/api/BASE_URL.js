5209aa4df4d3b00654ef9cbe3d29f8a698d18795
Fariz Darmaniar <haromy@MacBook-Pro.local>
2021-04-28 00:18:33 +0700

    change prod base url

--- src/api/BASE_URL.js
+++ src/api/BASE_URL.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://z6dstmoa8k.execute-api.ap-southeast-1.amazonaws.com/dev';
-// const BASE_URL = 'https://pa0cnz688b.execute-api.ap-southeast-1.amazonaws.com/live';
+// const BASE_URL = 'https://z6dstmoa8k.execute-api.ap-southeast-1.amazonaws.com/dev';
+const BASE_URL = 'https://pvhdnt8idd.execute-api.ap-southeast-1.amazonaws.com/live';
 
 export default BASE_URL;
