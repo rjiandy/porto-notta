f7e9903c8c0e1cd431bbd624e1f9d25325e6a2c9
Fariz Darmaniar <haromy@MacBook-Pro.local>
2021-04-30 08:22:39 +0700

    setup prod

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ import { Provider } from 'react-redux';
 import store from './stores';
 import MainNavigation from './navigations/Navigation';
 
-const DEV_APP_ID = '245b5b02-dfc4-48a7-9b19-752f6efb847b';
-// const PROD_APP_ID = 'a9543d8f-878d-400c-9a8a-d1ae1e36c14b';
+// const DEV_APP_ID = '245b5b02-dfc4-48a7-9b19-752f6efb847b';
+const PROD_APP_ID = 'a9543d8f-878d-400c-9a8a-d1ae1e36c14b';
 
 function App() {
   useEffect(() => {
-    OneSignal.setAppId(DEV_APP_ID);
+    OneSignal.setAppId(PROD_APP_ID);
     OneSignal.setLogLevel(6, 0);
     OneSignal.setRequiresUserPrivacyConsent(false);
     OneSignal.promptForPushNotificationsWithUserResponse(response => {
